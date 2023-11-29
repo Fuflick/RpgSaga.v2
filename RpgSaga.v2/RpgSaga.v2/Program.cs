caf3284bed5b7bb44f4eed84ec851a9e23f64bd0
Fuflick <stepan_belov_2016@mail.ru>
2023-11-29 03:07:33 +0300

    try to run project and some tests

--- RpgSaga.v2/RpgSaga.v2/Program.cs
+++ RpgSaga.v2/RpgSaga.v2/Program.cs
@@ -1,17 +1,16 @@
-﻿using System.Threading.Channels;
-
-namespace RpgSaga.v2;
+﻿namespace RpgSaga.v2;
 
 public class Program
 {
-    private readonly List<string> _immunities = new List<string>()
+    private static readonly List<string> Immunities = new List<string>()
     {
         "Fire",
         "Cold",
-        "Poison"
+        "Poison",
+        "No immunity"
     };
     
-    private readonly List<string> _names = new List<string>()
+    private static readonly List<string> Names = new List<string>()
     {
         "Ares",
         "Thor",
@@ -20,15 +19,19 @@ public class Program
         "Gideon",
         "Valeria",
         "Conan",
-        "Bellona",
+        "Bellina",
         "Ragnar",
         "Artemis"
     };
 
-    private static Random rnd = new Random();
+    private static Random _rnd = new Random();
     
     static void Main()
     {
-        Console.WriteLine("hui");
+        var bladeFactory = new BaldeFactory();
+        var blade = bladeFactory.CreateWeapon(_rnd.Next(1, 11));
+        var warriorFactory = new WarriorFactory();
+        var warrior = warriorFactory.CreateHero(Names[_rnd.Next(0, Names.Count)], _rnd.Next(10, 45), Immunities[_rnd.Next(0, Immunities.Count)], blade );
+        Console.WriteLine($"Name = {warrior.Name}, health = {warrior.Health}, immunity = {warrior.Immunity}, weapon is {warrior.Weapon.Type} and in's damage is {warrior.Weapon.Damage}");
     }
 }
