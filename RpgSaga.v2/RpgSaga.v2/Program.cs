e33e02c11cfc4e4140604a08e517bdd6b2049cba
Fuflick <stepan_belov_2016@mail.ru>
2023-12-02 02:33:07 +0300

    last base tests

--- RpgSaga.v2/RpgSaga.v2/Program.cs
+++ RpgSaga.v2/RpgSaga.v2/Program.cs
@@ -28,14 +28,47 @@ public class Program
     
     static void Main()
     {
-        var warriorFactory = new WarriorFactory();
-        var warrior = warriorFactory.CreateHero(Names[_rnd.Next(0, Names.Count)], _rnd.Next(10, 45), Immunities[_rnd.Next(0, Immunities.Count)]);
-        Console.WriteLine($"Name = {warrior.Name}, health = {warrior.Health}, immunity = {warrior.Immunity}, weapon is {warrior.Weapon.Type} and in's damage is {warrior.Weapon.Damage}");
+        while (true)
+        {
+            var rnd = new Random();
+            var warriorFactory = new WarriorFactory();
+            var warrior = warriorFactory.CreateHero(Names[_rnd.Next(0, Names.Count)], _rnd.Next(25, 50), Immunities[_rnd.Next(0, Immunities.Count)]);
+            Console.WriteLine($"Name = {warrior.Name}, health = {warrior.Health}, immunity = {warrior.Immunity}, weapon is {warrior.Weapon.Type} and in's damage is {warrior.Weapon.Damage}");
         
-        var archerFactory = new ArcherFactory();
-        var archer = archerFactory.CreateHero(Names[_rnd.Next(0, Names.Count)], _rnd.Next(10, 21), Immunities[_rnd.Next(0, Immunities.Count)]);
-        Console.WriteLine($"Name = {archer.Name}, health = {archer.Health}, immunity = {archer.Immunity}, weapon is {archer.Weapon.Type} and in's damage is {archer.Weapon.Damage}");
+            var archerFactory = new ArcherFactory();
+            var archer = archerFactory.CreateHero(Names[_rnd.Next(0, Names.Count)], _rnd.Next(10, 21), Immunities[_rnd.Next(0, Immunities.Count)]);
+            Console.WriteLine($"Name = {archer.Name}, health = {archer.Health}, immunity = {archer.Immunity}, weapon is {archer.Weapon.Type} and in's damage is {archer.Weapon.Damage}");
+
+            var wizrdFactory = new WizardFactory();
+            var wizard = warriorFactory.CreateHero(Names[_rnd.Next(0, Names.Count)], _rnd.Next(10, 15), Immunities[_rnd.Next(0, Immunities.Count)]);
+
+            var list = new List<IHero>()
+            {
+                warrior, wizard, archer,
+            };
+
+            var party = new List<IHero>();
+            party.Add(list[rnd.Next(0, list.Count)]);
+            party.Add(list[rnd.Next(0, list.Count)]);
+
+            if (party[0] != party[1])
+            {
+                while (party[0].Health > 0 && party[1].Health > 0)
+                {
+                    party[0].TakeDamage(party[1]);
+                    if (party[0].Health > 0)
+                    {
+                        party[1].TakeDamage(party[0]);
+                    }
+                    
+                    Thread.Sleep(3000);
+                }
+
+                Console.WriteLine("Fight end \n");
+            }
+        }
         
-        archer.TakeDamage(warrior);
+        
+        
     }
 }
